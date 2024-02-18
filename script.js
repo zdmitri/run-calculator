eaffd6361edf5d0cff4bd4e9acbbd6b9952f29df
Dmytro Zadorozhnyi <dmitriiz@gmail.com>
2024-02-18 23:19:59 +0200

    added function getTime to check user entry in time fields

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
+let Time;
+
+// Function to calculate total time (in seconds) from the data input by user
+
+function getTime() {
+
+    //declare time variables, get data from input fields
+    let Hours = document.getElementById('inputHours').value;
+    let Minutes = document.getElementById('inputMinutes').value;
+    let Seconds = document.getElementById('inputSeconds').value;
+
+    //Check if user made any entry. if NO assign zero value
+    Hours = Hours == '' ? 0 : parseFloat(Hours);
+    Minutes = Minutes == '' ? 0 : parseFloat(Minutes);
+    Seconds = Seconds == '' ? 0 : parseFloat(Seconds);
+    console.log(Hours, typeof(Hours));
+    console.log(Minutes, typeof(Minutes));
+    console.log(Seconds, typeof(Seconds));
+
+    // declare Time variable and count it in seconds
+    Time = (Hours * 3600) + (Minutes * 60) + Seconds;
+    console.log(`Total time in seconds is: ${Time} ${typeof (Time)}`);
+    //return Time;
+}
+
 function calculatePace() {
     
-    // declare variables, get data from input fields
+    getTime();
+
     let Distance = parseFloat(document.getElementById('inputDistance').value);
-    let Hours = parseFloat(document.getElementById('inputHours').value);
-    let Minutes = parseFloat(document.getElementById('inputMinutes').value);
-    let Seconds = parseFloat(document.getElementById('inputSeconds').value);
-    // declare Time variable and count it in seconds
-    let Time = (Hours * 3600) + (Minutes * 60) + Seconds;
-    // declare Pace 
     let Pace = (Time / Distance) / 60; // pace in min/km
     paceMinutes = Math.floor(Pace);
     paceSeconds = (Pace - Math.floor(Pace)) * 60;
         
     console.log(`Distance is ${Distance} KM`, typeof(Distance));
-    console.log(`Total time in seconds is: ${Time} ${typeof (Time)}`);
     console.log(`Pace is ${Pace} min/km`);
     console.log(`Pace is ${paceMinutes}:${paceSeconds} min:sec/km`);
     console.log(`Pace is ${paceMinutes}:${paceSeconds.toFixed(0)} per km (${paceMinutes}:${paceSeconds.toFixed(3)})`);
     
-    alert(`Click! Target pace should be ${paceMinutes}:${paceSeconds.toFixed(0)} per km (${paceMinutes}:${paceSeconds.toFixed(3)})`);
-
+    alert(`Click! Target pace should be ${paceMinutes} minutes ${paceSeconds.toFixed(0)} seconds per km (${paceMinutes}:${paceSeconds.toFixed(3)})`);
 };
 
 function clickCalculate() {
