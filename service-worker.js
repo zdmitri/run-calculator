083b349549553f19be29897f8920cdadd2450b96
Dmytro Zadorozhnyi <dmitriiz@gmail.com>
2024-03-14 14:05:13 +0200

    started adding PWA functionality

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('my-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                /* Add more files you want to cache here */
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
